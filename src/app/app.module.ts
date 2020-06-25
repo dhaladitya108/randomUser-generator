7a1a459cb6daf4c4a43d22d8c7d6a2c5fc476f1f
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-06-25 08:33:23 +0530

    🤡 installed some the required stuffs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { from } from 'rxjs';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent
-  ],
+  declarations: [AppComponent, CardComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
